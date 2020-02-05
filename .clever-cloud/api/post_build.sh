efde519fc7d615215324f9710ce1ebddf232daca
Pierre Tondereau <pierre.tondereau@protonmail.com>
2020-02-05 14:47:59 +0100

    Test post build.

--- .clever-cloud/api/post_build.sh
+++ .clever-cloud/api/post_build.sh
@@ -1,3 +1,4 @@
-./backend/bin/console doctrine:migrations:migrate --no-interaction
-./backend/bin/console cache:clear --no-interaction
-./backend/bin/console cache:warmup --no-interaction
+cd ./backend
+bin/console doctrine:migrations:migrate --no-interaction
+bin/console cache:clear --no-interaction
+bin/console cache:warmup --no-interaction
