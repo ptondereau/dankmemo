399e0d78473e5cdbc3460dfb8f28e5f16b2fa6b4
Pierre Tondereau <pierre.tondereau@protonmail.com>
2020-02-05 21:42:41 +0100

    Fix prod frontend build.

--- frontend/src/context/index.ts
+++ frontend/src/context/index.ts
@@ -1,8 +1,4 @@
 import React from 'react';
-// @ts-ignore
-import makeInspectable from 'mobx-devtools-mst';
 import { GameStore } from '../stores/GameStore';
 
-export const GameContext = React.createContext(
-  makeInspectable(new GameStore())
-);
+export const GameContext = React.createContext(new GameStore());
